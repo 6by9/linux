89f5952b139903a7cedff22247e9d8af629e315d
Dave Stevenson <dave.stevenson@raspberrypi.com>
2022-10-03 15:00:55 +0100

    Revert "media: i2c: Add pm_runtime support to ov7251"
    This reverts commit a24fa68bd47ee44f077c9ba30e18be455ac97f2b.

--- drivers/media/i2c/ov7251.c
+++ drivers/media/i2c/ov7251.c
@@ -15,7 +15,6 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/mod_devicetable.h>
-#include <linux/pm_runtime.h>
 #include <linux/regulator/consumer.h>
 #include <linux/slab.h>
 #include <linux/types.h>
@@ -885,24 +884,6 @@ static void ov7251_set_power_off(struct ov7251 *ov7251)
 	ov7251_regulators_disable(ov7251);
 }
 
-static int __maybe_unused ov7251_sensor_suspend(struct device *dev)
-{
-	struct v4l2_subdev *sd = dev_get_drvdata(dev);
-	struct ov7251 *ov7251 = to_ov7251(sd);
-
-	ov7251_set_power_off(ov7251);
-
-	return 0;
-}
-
-static int __maybe_unused ov7251_sensor_resume(struct device *dev)
-{
-	struct v4l2_subdev *sd = dev_get_drvdata(dev);
-	struct ov7251 *ov7251 = to_ov7251(sd);
-
-	return ov7251_set_power_on(ov7251);
-}
-
 static int ov7251_s_power(struct v4l2_subdev *sd, int on)
 {
 	struct ov7251 *ov7251 = to_ov7251(sd);
@@ -1004,7 +985,7 @@ static int ov7251_s_ctrl(struct v4l2_ctrl *ctrl)
 
 	/* v4l2_ctrl_lock() locks our mutex */
 
-	if (!pm_runtime_get_if_in_use(ov7251->dev))
+	if (!ov7251->power_on)
 		return 0;
 
 	switch (ctrl->id) {
@@ -1027,8 +1008,6 @@ static int ov7251_s_ctrl(struct v4l2_ctrl *ctrl)
 		ret = -EINVAL;
 		break;
 	}
-
-	pm_runtime_put(ov7251->dev);
 
 	return ret;
 }
@@ -1282,15 +1261,10 @@ static int ov7251_s_stream(struct v4l2_subdev *subdev, int enable)
 	mutex_lock(&ov7251->lock);
 
 	if (enable) {
-		ret = pm_runtime_get_sync(ov7251->dev);
-		if (ret < 0)
-			return ret;
-
 		ret = ov7251_pll_configure(ov7251);
-		if (ret) {
-			dev_err(ov7251->dev, "error configuring PLLs\n");
-			goto err_power_down;
-		}
+		if (ret)
+			return dev_err_probe(ov7251->dev, ret,
+					     "error configuring PLLs\n");
 
 		ret = ov7251_set_register_array(ov7251,
 					ov7251->current_mode->data,
@@ -1299,29 +1273,23 @@ static int ov7251_s_stream(struct v4l2_subdev *subdev, int enable)
 			dev_err(ov7251->dev, "could not set mode %dx%d\n",
 				ov7251->current_mode->width,
 				ov7251->current_mode->height);
-			goto err_power_down;
+			goto exit;
 		}
 		ret = __v4l2_ctrl_handler_setup(&ov7251->ctrls);
 		if (ret < 0) {
 			dev_err(ov7251->dev, "could not sync v4l2 controls\n");
-			goto err_power_down;
+			goto exit;
 		}
 		ret = ov7251_write_reg(ov7251, OV7251_SC_MODE_SELECT,
 				       OV7251_SC_MODE_SELECT_STREAMING);
-		if (ret)
-			goto err_power_down;
 	} else {
 		ret = ov7251_write_reg(ov7251, OV7251_SC_MODE_SELECT,
 				       OV7251_SC_MODE_SELECT_SW_STANDBY);
-		pm_runtime_put(ov7251->dev);
 	}
 
+exit:
 	mutex_unlock(&ov7251->lock);
-	return ret;
 
-err_power_down:
-	pm_runtime_put_noidle(ov7251->dev);
-	mutex_unlock(&ov7251->lock);
 	return ret;
 }
 
@@ -1647,24 +1615,23 @@ static int ov7251_probe(struct i2c_client *client)
 		goto free_ctrl;
 	}
 
-	ret = ov7251_set_power_on(ov7251);
-	if (ret)
+	ret = ov7251_s_power(&ov7251->sd, true);
+	if (ret < 0) {
+		dev_err(dev, "could not power up OV7251\n");
 		goto free_entity;
+	}
 
 	ret = ov7251_detect_chip(ov7251);
 	if (ret)
 		goto power_down;
 
-	pm_runtime_set_active(&client->dev);
-	pm_runtime_get_noresume(&client->dev);
-	pm_runtime_enable(&client->dev);
 
 	ret = ov7251_read_reg(ov7251, OV7251_PRE_ISP_00,
 			      &ov7251->pre_isp_00);
 	if (ret < 0) {
 		dev_err(dev, "could not read test pattern value\n");
 		ret = -ENODEV;
-		goto err_pm_runtime;
+		goto power_down;
 	}
 
 	ret = ov7251_read_reg(ov7251, OV7251_TIMING_FORMAT1,
@@ -1672,7 +1639,7 @@ static int ov7251_probe(struct i2c_client *client)
 	if (ret < 0) {
 		dev_err(dev, "could not read vflip value\n");
 		ret = -ENODEV;
-		goto err_pm_runtime;
+		goto power_down;
 	}
 
 	ret = ov7251_read_reg(ov7251, OV7251_TIMING_FORMAT2,
@@ -1680,12 +1647,10 @@ static int ov7251_probe(struct i2c_client *client)
 	if (ret < 0) {
 		dev_err(dev, "could not read hflip value\n");
 		ret = -ENODEV;
-		goto err_pm_runtime;
+		goto power_down;
 	}
 
-	pm_runtime_set_autosuspend_delay(&client->dev, 1000);
-	pm_runtime_use_autosuspend(&client->dev);
-	pm_runtime_put_autosuspend(&client->dev);
+	ov7251_s_power(&ov7251->sd, false);
 
 	ret = v4l2_async_register_subdev(&ov7251->sd);
 	if (ret < 0) {
@@ -1697,9 +1662,6 @@ static int ov7251_probe(struct i2c_client *client)
 
 	return 0;
 
-err_pm_runtime:
-	pm_runtime_disable(ov7251->dev);
-	pm_runtime_put_noidle(ov7251->dev);
 power_down:
 	ov7251_s_power(&ov7251->sd, false);
 free_entity:
@@ -1721,17 +1683,8 @@ static int ov7251_remove(struct i2c_client *client)
 	v4l2_ctrl_handler_free(&ov7251->ctrls);
 	mutex_destroy(&ov7251->lock);
 
-	pm_runtime_disable(ov7251->dev);
-	if (!pm_runtime_status_suspended(ov7251->dev))
-		ov7251_set_power_off(ov7251);
-	pm_runtime_set_suspended(ov7251->dev);
-
 	return 0;
 }
-
-static const struct dev_pm_ops ov7251_pm_ops = {
-	SET_RUNTIME_PM_OPS(ov7251_sensor_suspend, ov7251_sensor_resume, NULL)
-};
 
 static const struct of_device_id ov7251_of_match[] = {
 	{ .compatible = "ovti,ov7251" },
@@ -1750,7 +1703,6 @@ static struct i2c_driver ov7251_i2c_driver = {
 		.of_match_table = ov7251_of_match,
 		.acpi_match_table = ov7251_acpi_match,
 		.name  = "ov7251",
-		.pm = &ov7251_pm_ops,
 	},
 	.probe_new  = ov7251_probe,
 	.remove = ov7251_remove,
