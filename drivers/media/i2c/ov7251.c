eee9036d3de6ece5e0e226ac4841b10df8d54f9d
Dave Stevenson <dave.stevenson@raspberrypi.com>
2022-10-03 17:23:21 +0100

    media: i2c: ov7251: Fixup VBLANK interactions with set_frame_interval
    The driver supported both V4L2_CTRL_VBLANK and set_frame_interval
    to alter registers 0x380e/f (VTS) to set the frame rate. As the
    VBLANK control would always be set after the mode table had been
    sent, that setting would always take precedence.
    
    Remove the duplication of mode table, break out the frame interval
    options, and set the vblank control from set_frame_interval.
    
    Note that as only discrete frame intervals are enumerated, changes
    to the vblank control will NOT be reflected in the value returned
    by get_frame_interval.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/media/i2c/ov7251.c
+++ drivers/media/i2c/ov7251.c
@@ -63,7 +63,6 @@
 
 #define OV7251_FIXED_PPL		928
 #define OV7251_TIMING_VTS_REG		0x380e
-#define OV7251_TIMING_MIN_VTS		1
 #define OV7251_TIMING_MAX_VTS		0xffff
 #define OV7251_INTEGRATION_MARGIN	20
 
@@ -72,17 +71,22 @@ struct reg_value {
 	u8 val;
 };
 
+struct ov7251_fi_info {
+	struct v4l2_fract timeperframe;
+	u32 vts;
+};
+
 struct ov7251_mode_info {
 	u32 width;
 	u32 height;
-	u32 vts;
 	const struct reg_value *data;
 	u32 data_size;
 	u32 pixel_clock;
 	u32 link_freq;
 	u16 exposure_max;
 	u16 exposure_def;
-	struct v4l2_fract timeperframe;
+	u32 vts_min;
+	u32 vts_def;
 };
 
 struct ov7251_pll1_cfg {
@@ -140,6 +144,7 @@ struct ov7251 {
 	const struct ov7251_pll_cfgs *pll_cfgs;
 	enum supported_link_freqs link_freq_idx;
 	const struct ov7251_mode_info *current_mode;
+	const struct ov7251_fi_info *current_fi;
 
 	struct v4l2_ctrl_handler ctrls;
 	struct v4l2_ctrl *pixel_clock;
@@ -239,7 +244,7 @@ static const struct reg_value ov7251_global_init_setting[] = {
 	{ 0x303b, 0x02 },
 };
 
-static const struct reg_value ov7251_setting_vga_30fps[] = {
+static const struct reg_value ov7251_setting_vga[] = {
 	{ 0x3005, 0x00 },
 	{ 0x3012, 0xc0 },
 	{ 0x3013, 0xd2 },
@@ -297,264 +302,6 @@ static const struct reg_value ov7251_setting_vga_30fps[] = {
 	{ 0x380b, 0xe0 }, /* height low */
 	{ 0x380c, 0x03 }, /* total horiz timing high */
 	{ 0x380d, 0xa0 }, /* total horiz timing low */
-	{ 0x380e, 0x06 }, /* total vertical timing high */
-	{ 0x380f, 0xbc }, /* total vertical timing low */
-	{ 0x3810, 0x00 },
-	{ 0x3811, 0x04 },
-	{ 0x3812, 0x00 },
-	{ 0x3813, 0x05 },
-	{ 0x3814, 0x11 },
-	{ 0x3815, 0x11 },
-	{ 0x3820, 0x40 },
-	{ 0x3821, 0x00 },
-	{ 0x382f, 0x0e },
-	{ 0x3832, 0x00 },
-	{ 0x3833, 0x05 },
-	{ 0x3834, 0x00 },
-	{ 0x3835, 0x0c },
-	{ 0x3837, 0x00 },
-	{ 0x3b80, 0x00 },
-	{ 0x3b81, 0xa5 },
-	{ 0x3b82, 0x10 },
-	{ 0x3b83, 0x00 },
-	{ 0x3b84, 0x08 },
-	{ 0x3b85, 0x00 },
-	{ 0x3b86, 0x01 },
-	{ 0x3b87, 0x00 },
-	{ 0x3b88, 0x00 },
-	{ 0x3b89, 0x00 },
-	{ 0x3b8a, 0x00 },
-	{ 0x3b8b, 0x05 },
-	{ 0x3b8c, 0x00 },
-	{ 0x3b8d, 0x00 },
-	{ 0x3b8e, 0x00 },
-	{ 0x3b8f, 0x1a },
-	{ 0x3b94, 0x05 },
-	{ 0x3b95, 0xf2 },
-	{ 0x3b96, 0x40 },
-	{ 0x3c00, 0x89 },
-	{ 0x3c01, 0x63 },
-	{ 0x3c02, 0x01 },
-	{ 0x3c03, 0x00 },
-	{ 0x3c04, 0x00 },
-	{ 0x3c05, 0x03 },
-	{ 0x3c06, 0x00 },
-	{ 0x3c07, 0x06 },
-	{ 0x3c0c, 0x01 },
-	{ 0x3c0d, 0xd0 },
-	{ 0x3c0e, 0x02 },
-	{ 0x3c0f, 0x0a },
-	{ 0x4001, 0x42 },
-	{ 0x4004, 0x04 },
-	{ 0x4005, 0x00 },
-	{ 0x404e, 0x01 },
-	{ 0x4300, 0xff },
-	{ 0x4301, 0x00 },
-	{ 0x4315, 0x00 },
-	{ 0x4501, 0x48 },
-	{ 0x4600, 0x00 },
-	{ 0x4601, 0x4e },
-	{ 0x4801, 0x0f },
-	{ 0x4806, 0x0f },
-	{ 0x4819, 0xaa },
-	{ 0x4823, 0x3e },
-	{ 0x4837, 0x19 },
-	{ 0x4a0d, 0x00 },
-	{ 0x4a47, 0x7f },
-	{ 0x4a49, 0xf0 },
-	{ 0x4a4b, 0x30 },
-	{ 0x5000, 0x85 },
-	{ 0x5001, 0x80 },
-};
-
-static const struct reg_value ov7251_setting_vga_60fps[] = {
-	{ 0x3005, 0x00 },
-	{ 0x3012, 0xc0 },
-	{ 0x3013, 0xd2 },
-	{ 0x3014, 0x04 },
-	{ 0x3016, 0x10 },
-	{ 0x3017, 0x00 },
-	{ 0x3018, 0x00 },
-	{ 0x301a, 0x00 },
-	{ 0x301b, 0x00 },
-	{ 0x301c, 0x00 },
-	{ 0x3023, 0x05 },
-	{ 0x3037, 0xf0 },
-	{ 0x3106, 0xda },
-	{ 0x3503, 0x07 },
-	{ 0x3509, 0x10 },
-	{ 0x3600, 0x1c },
-	{ 0x3602, 0x62 },
-	{ 0x3620, 0xb7 },
-	{ 0x3622, 0x04 },
-	{ 0x3626, 0x21 },
-	{ 0x3627, 0x30 },
-	{ 0x3630, 0x44 },
-	{ 0x3631, 0x35 },
-	{ 0x3634, 0x60 },
-	{ 0x3636, 0x00 },
-	{ 0x3662, 0x01 },
-	{ 0x3663, 0x70 },
-	{ 0x3664, 0x50 },
-	{ 0x3666, 0x0a },
-	{ 0x3669, 0x1a },
-	{ 0x366a, 0x00 },
-	{ 0x366b, 0x50 },
-	{ 0x3673, 0x01 },
-	{ 0x3674, 0xff },
-	{ 0x3675, 0x03 },
-	{ 0x3705, 0xc1 },
-	{ 0x3709, 0x40 },
-	{ 0x373c, 0x08 },
-	{ 0x3742, 0x00 },
-	{ 0x3757, 0xb3 },
-	{ 0x3788, 0x00 },
-	{ 0x37a8, 0x01 },
-	{ 0x37a9, 0xc0 },
-	{ 0x3800, 0x00 },
-	{ 0x3801, 0x04 },
-	{ 0x3802, 0x00 },
-	{ 0x3803, 0x04 },
-	{ 0x3804, 0x02 },
-	{ 0x3805, 0x8b },
-	{ 0x3806, 0x01 },
-	{ 0x3807, 0xeb },
-	{ 0x3808, 0x02 }, /* width high */
-	{ 0x3809, 0x80 }, /* width low */
-	{ 0x380a, 0x01 }, /* height high */
-	{ 0x380b, 0xe0 }, /* height low */
-	{ 0x380c, 0x03 }, /* total horiz timing high */
-	{ 0x380d, 0xa0 }, /* total horiz timing low */
-	{ 0x380e, 0x03 }, /* total vertical timing high */
-	{ 0x380f, 0x5c }, /* total vertical timing low */
-	{ 0x3810, 0x00 },
-	{ 0x3811, 0x04 },
-	{ 0x3812, 0x00 },
-	{ 0x3813, 0x05 },
-	{ 0x3814, 0x11 },
-	{ 0x3815, 0x11 },
-	{ 0x3820, 0x40 },
-	{ 0x3821, 0x00 },
-	{ 0x382f, 0x0e },
-	{ 0x3832, 0x00 },
-	{ 0x3833, 0x05 },
-	{ 0x3834, 0x00 },
-	{ 0x3835, 0x0c },
-	{ 0x3837, 0x00 },
-	{ 0x3b80, 0x00 },
-	{ 0x3b81, 0xa5 },
-	{ 0x3b82, 0x10 },
-	{ 0x3b83, 0x00 },
-	{ 0x3b84, 0x08 },
-	{ 0x3b85, 0x00 },
-	{ 0x3b86, 0x01 },
-	{ 0x3b87, 0x00 },
-	{ 0x3b88, 0x00 },
-	{ 0x3b89, 0x00 },
-	{ 0x3b8a, 0x00 },
-	{ 0x3b8b, 0x05 },
-	{ 0x3b8c, 0x00 },
-	{ 0x3b8d, 0x00 },
-	{ 0x3b8e, 0x00 },
-	{ 0x3b8f, 0x1a },
-	{ 0x3b94, 0x05 },
-	{ 0x3b95, 0xf2 },
-	{ 0x3b96, 0x40 },
-	{ 0x3c00, 0x89 },
-	{ 0x3c01, 0x63 },
-	{ 0x3c02, 0x01 },
-	{ 0x3c03, 0x00 },
-	{ 0x3c04, 0x00 },
-	{ 0x3c05, 0x03 },
-	{ 0x3c06, 0x00 },
-	{ 0x3c07, 0x06 },
-	{ 0x3c0c, 0x01 },
-	{ 0x3c0d, 0xd0 },
-	{ 0x3c0e, 0x02 },
-	{ 0x3c0f, 0x0a },
-	{ 0x4001, 0x42 },
-	{ 0x4004, 0x04 },
-	{ 0x4005, 0x00 },
-	{ 0x404e, 0x01 },
-	{ 0x4300, 0xff },
-	{ 0x4301, 0x00 },
-	{ 0x4315, 0x00 },
-	{ 0x4501, 0x48 },
-	{ 0x4600, 0x00 },
-	{ 0x4601, 0x4e },
-	{ 0x4801, 0x0f },
-	{ 0x4806, 0x0f },
-	{ 0x4819, 0xaa },
-	{ 0x4823, 0x3e },
-	{ 0x4837, 0x19 },
-	{ 0x4a0d, 0x00 },
-	{ 0x4a47, 0x7f },
-	{ 0x4a49, 0xf0 },
-	{ 0x4a4b, 0x30 },
-	{ 0x5000, 0x85 },
-	{ 0x5001, 0x80 },
-};
-
-static const struct reg_value ov7251_setting_vga_90fps[] = {
-	{ 0x3005, 0x00 },
-	{ 0x3012, 0xc0 },
-	{ 0x3013, 0xd2 },
-	{ 0x3014, 0x04 },
-	{ 0x3016, 0x10 },
-	{ 0x3017, 0x00 },
-	{ 0x3018, 0x00 },
-	{ 0x301a, 0x00 },
-	{ 0x301b, 0x00 },
-	{ 0x301c, 0x00 },
-	{ 0x3023, 0x05 },
-	{ 0x3037, 0xf0 },
-	{ 0x3106, 0xda },
-	{ 0x3503, 0x07 },
-	{ 0x3509, 0x10 },
-	{ 0x3600, 0x1c },
-	{ 0x3602, 0x62 },
-	{ 0x3620, 0xb7 },
-	{ 0x3622, 0x04 },
-	{ 0x3626, 0x21 },
-	{ 0x3627, 0x30 },
-	{ 0x3630, 0x44 },
-	{ 0x3631, 0x35 },
-	{ 0x3634, 0x60 },
-	{ 0x3636, 0x00 },
-	{ 0x3662, 0x01 },
-	{ 0x3663, 0x70 },
-	{ 0x3664, 0x50 },
-	{ 0x3666, 0x0a },
-	{ 0x3669, 0x1a },
-	{ 0x366a, 0x00 },
-	{ 0x366b, 0x50 },
-	{ 0x3673, 0x01 },
-	{ 0x3674, 0xff },
-	{ 0x3675, 0x03 },
-	{ 0x3705, 0xc1 },
-	{ 0x3709, 0x40 },
-	{ 0x373c, 0x08 },
-	{ 0x3742, 0x00 },
-	{ 0x3757, 0xb3 },
-	{ 0x3788, 0x00 },
-	{ 0x37a8, 0x01 },
-	{ 0x37a9, 0xc0 },
-	{ 0x3800, 0x00 },
-	{ 0x3801, 0x04 },
-	{ 0x3802, 0x00 },
-	{ 0x3803, 0x04 },
-	{ 0x3804, 0x02 },
-	{ 0x3805, 0x8b },
-	{ 0x3806, 0x01 },
-	{ 0x3807, 0xeb },
-	{ 0x3808, 0x02 }, /* width high */
-	{ 0x3809, 0x80 }, /* width low */
-	{ 0x380a, 0x01 }, /* height high */
-	{ 0x380b, 0xe0 }, /* height low */
-	{ 0x380c, 0x03 }, /* total horiz timing high */
-	{ 0x380d, 0xa0 }, /* total horiz timing low */
-	{ 0x380e, 0x02 }, /* total vertical timing high */
-	{ 0x380f, 0x3c }, /* total vertical timing low */
 	{ 0x3810, 0x00 },
 	{ 0x3811, 0x04 },
 	{ 0x3812, 0x00 },
@@ -642,41 +389,36 @@ static const struct ov7251_mode_info ov7251_mode_info_data[] = {
 	{
 		.width = 640,
 		.height = 480,
-		.vts = 1724,
-		.data = ov7251_setting_vga_30fps,
-		.data_size = ARRAY_SIZE(ov7251_setting_vga_30fps),
+		.data = ov7251_setting_vga,
+		.data_size = ARRAY_SIZE(ov7251_setting_vga),
 		.exposure_max = 1704,
 		.exposure_def = 504,
+		.vts_min = 572,		/* 90fps */
+		.vts_def = 1724,	/* 30fps */
+	},
+};
+
+static const struct ov7251_fi_info ov7251_fi_info_data[] = {
+	{
 		.timeperframe = {
 			.numerator = 100,
 			.denominator = 3000
-		}
+		},
+		.vts = 1724,
 	},
 	{
-		.width = 640,
-		.height = 480,
-		.vts = 860,
-		.data = ov7251_setting_vga_60fps,
-		.data_size = ARRAY_SIZE(ov7251_setting_vga_60fps),
-		.exposure_max = 840,
-		.exposure_def = 504,
 		.timeperframe = {
 			.numerator = 100,
 			.denominator = 6014
-		}
+		},
+		.vts = 860,
 	},
 	{
-		.width = 640,
-		.height = 480,
-		.vts = 572,
-		.data = ov7251_setting_vga_90fps,
-		.data_size = ARRAY_SIZE(ov7251_setting_vga_90fps),
-		.exposure_max = 552,
-		.exposure_def = 504,
 		.timeperframe = {
 			.numerator = 100,
 			.denominator = 9043
-		}
+		},
+		.vts = 572,
 	},
 };
 
@@ -1116,13 +858,9 @@ static int ov7251_enum_frame_ival(struct v4l2_subdev *subdev,
 	unsigned int index = fie->index;
 	unsigned int i;
 
-	for (i = 0; i < ARRAY_SIZE(ov7251_mode_info_data); i++) {
-		if (fie->width != ov7251_mode_info_data[i].width ||
-		    fie->height != ov7251_mode_info_data[i].height)
-			continue;
-
+	for (i = 0; i < ARRAY_SIZE(ov7251_fi_info_data); i++) {
 		if (index-- == 0) {
-			fie->interval = ov7251_mode_info_data[i].timeperframe;
+			fie->interval = ov7251_fi_info_data[i].timeperframe;
 			return 0;
 		}
 	}
@@ -1181,23 +919,18 @@ static inline u32 avg_fps(const struct v4l2_fract *t)
 	return (t->denominator + (t->numerator >> 1)) / t->numerator;
 }
 
-static const struct ov7251_mode_info *
-ov7251_find_mode_by_ival(struct ov7251 *ov7251, struct v4l2_fract *timeperframe)
+static const struct ov7251_fi_info *
+ov7251_find_interval_by_ival(struct ov7251 *ov7251, struct v4l2_fract *timeperframe)
 {
-	const struct ov7251_mode_info *mode = ov7251->current_mode;
 	unsigned int fps_req = avg_fps(timeperframe);
 	unsigned int max_dist_match = (unsigned int) -1;
 	unsigned int i, n = 0;
 
-	for (i = 0; i < ARRAY_SIZE(ov7251_mode_info_data); i++) {
+	for (i = 0; i < ARRAY_SIZE(ov7251_fi_info_data); i++) {
 		unsigned int dist;
 		unsigned int fps_tmp;
 
-		if (mode->width != ov7251_mode_info_data[i].width ||
-		    mode->height != ov7251_mode_info_data[i].height)
-			continue;
-
-		fps_tmp = avg_fps(&ov7251_mode_info_data[i].timeperframe);
+		fps_tmp = avg_fps(&ov7251_fi_info_data[i].timeperframe);
 
 		dist = abs(fps_req - fps_tmp);
 
@@ -1207,7 +940,7 @@ ov7251_find_mode_by_ival(struct ov7251 *ov7251, struct v4l2_fract *timeperframe)
 		}
 	}
 
-	return &ov7251_mode_info_data[n];
+	return &ov7251_fi_info_data[n];
 }
 
 static int ov7251_set_format(struct v4l2_subdev *sd,
@@ -1216,7 +949,7 @@ static int ov7251_set_format(struct v4l2_subdev *sd,
 {
 	struct ov7251 *ov7251 = to_ov7251(sd);
 	struct v4l2_mbus_framefmt *__format;
-	int vblank_max, vblank_def;
+	int vblank_max, vblank_min, vblank_def;
 	struct v4l2_rect *__crop;
 	const struct ov7251_mode_info *new_mode;
 	int ret = 0;
@@ -1247,10 +980,11 @@ static int ov7251_set_format(struct v4l2_subdev *sd,
 			goto exit;
 
 		vblank_max = OV7251_TIMING_MAX_VTS - new_mode->height;
-		vblank_def = new_mode->vts - new_mode->height;
+		vblank_min = new_mode->vts_min - new_mode->height;
+		vblank_def = new_mode->vts_def - new_mode->height;
 		ret = __v4l2_ctrl_modify_range(ov7251->vblank,
-					       OV7251_TIMING_MIN_VTS,
-					       vblank_max, 1, vblank_def);
+					       vblank_min, vblank_max, 1,
+					       vblank_def);
 		if (ret < 0)
 			goto exit;
 
@@ -1384,7 +1118,7 @@ static int ov7251_get_frame_interval(struct v4l2_subdev *subdev,
 	struct ov7251 *ov7251 = to_ov7251(subdev);
 
 	mutex_lock(&ov7251->lock);
-	fi->interval = ov7251->current_mode->timeperframe;
+	fi->interval = ov7251->current_fi->timeperframe;
 	mutex_unlock(&ov7251->lock);
 
 	return 0;
@@ -1393,29 +1127,24 @@ static int ov7251_get_frame_interval(struct v4l2_subdev *subdev,
 static int ov7251_set_frame_interval(struct v4l2_subdev *subdev,
 				     struct v4l2_subdev_frame_interval *fi)
 {
+	const struct ov7251_fi_info *new_fi;
 	struct ov7251 *ov7251 = to_ov7251(subdev);
-	const struct ov7251_mode_info *new_mode;
 	int ret = 0;
 
 	mutex_lock(&ov7251->lock);
-	new_mode = ov7251_find_mode_by_ival(ov7251, &fi->interval);
+	new_fi = ov7251_find_interval_by_ival(ov7251, &fi->interval);
 
-	if (new_mode != ov7251->current_mode) {
-		ret = __v4l2_ctrl_modify_range(ov7251->exposure,
-					       1, new_mode->exposure_max,
-					       1, new_mode->exposure_def);
+	if (new_fi != ov7251->current_fi) {
+		/* Setting VBLANK will also update exposure ranges */
+		ret = __v4l2_ctrl_s_ctrl(ov7251->vblank,
+					 new_fi->vts);
 		if (ret < 0)
 			goto exit;
 
-		ret = __v4l2_ctrl_s_ctrl(ov7251->exposure,
-					 new_mode->exposure_def);
-		if (ret < 0)
-			goto exit;
-
-		ov7251->current_mode = new_mode;
+		ov7251->current_fi = new_fi;
 	}
 
-	fi->interval = ov7251->current_mode->timeperframe;
+	fi->interval = ov7251->current_fi->timeperframe;
 
 exit:
 	mutex_unlock(&ov7251->lock);
@@ -1529,7 +1258,7 @@ static int ov7251_detect_chip(struct ov7251 *ov7251)
 static int ov7251_init_ctrls(struct ov7251 *ov7251)
 {
 	struct v4l2_fwnode_device_properties props;
-	int vblank_max, vblank_def;
+	int vblank_max, vblank_min, vblank_def;
 	s64 pixel_rate;
 	int hblank;
 	int ret;
@@ -1575,10 +1304,11 @@ static int ov7251_init_ctrls(struct ov7251 *ov7251)
 		ov7251->hblank->flags |= V4L2_CTRL_FLAG_READ_ONLY;
 
 	vblank_max = OV7251_TIMING_MAX_VTS - ov7251->current_mode->height;
-	vblank_def = ov7251->current_mode->vts - ov7251->current_mode->height;
+	vblank_min = ov7251->current_mode->vts_min - ov7251->current_mode->height;
+	vblank_def = ov7251->current_mode->vts_def - ov7251->current_mode->height;
 	ov7251->vblank = v4l2_ctrl_new_std(&ov7251->ctrls, &ov7251_ctrl_ops,
 					   V4L2_CID_VBLANK,
-					   OV7251_TIMING_MIN_VTS, vblank_max, 1,
+					   vblank_min, vblank_max, 1,
 					   vblank_def);
 
 	ret = v4l2_fwnode_device_parse(ov7251->dev, &props);
@@ -1684,6 +1414,7 @@ static int ov7251_probe(struct i2c_client *client)
 	mutex_init(&ov7251->lock);
 
 	ov7251->current_mode = &ov7251_mode_info_data[0];
+	ov7251->current_fi = &ov7251_fi_info_data[0];
 	ret = ov7251_init_ctrls(ov7251);
 	if (ret) {
 		dev_err_probe(dev, ret, "error during v4l2 ctrl init\n");
