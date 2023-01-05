3894eb3413ab2ad0d24f7dcf4601875e964175af
Dave Stevenson <dave.stevenson@raspberrypi.com>
2023-01-05 11:20:37 +0000

    Input: ads7846: Add spi_device_id entries for all compatibles
    The SPI framework still matches devices using the
    spi_device_id as well as compatible strings, therefore add
    the table for all the defined compatibles.
    
    This silences the warnings introduced with commit
    5fa6863ba692 ("spi: Check we have a spi_device_id for each DT compatible")
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/input/touchscreen/ads7846.c
+++ drivers/input/touchscreen/ads7846.c
@@ -1135,6 +1135,16 @@ static const struct of_device_id ads7846_dt_ids[] = {
 };
 MODULE_DEVICE_TABLE(of, ads7846_dt_ids);
 
+static const struct spi_device_id ads7846_id[] = {
+	{ "tsc2046", 0 },
+	{ "ads7843", 0 },
+	{ "ads7845", 0 },
+	{ "ads7846", 0 },
+	{ "ads7873", 0 },
+	{ }
+};
+MODULE_DEVICE_TABLE(spi, ads7846_id);
+
 static const struct ads7846_platform_data *ads7846_probe_dt(struct device *dev)
 {
 	struct ads7846_platform_data *pdata;
@@ -1474,6 +1484,7 @@ static struct spi_driver ads7846_driver = {
 		.pm	= &ads7846_pm,
 		.of_match_table = of_match_ptr(ads7846_dt_ids),
 	},
+	.id_table	= ads7846_id,
 	.probe		= ads7846_probe,
 	.remove		= ads7846_remove,
 };
